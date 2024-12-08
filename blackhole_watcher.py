4e0b6f03dba711a420e7f32bad9b99fbcb67dfaf
Keerthan <keerthan@arrapps.keets.local>
2024-12-08 17:36:04 -0600

    Initial commit with Docker project

--- blackhole_watcher.py
+++ blackhole_watcher.py
@@ -1,48 +1,27 @@
 import asyncio
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchfiles import awatch
 from blackhole import on_created, getPath
 
-class BlackholeHandler(FileSystemEventHandler):
+class BlackholeHandler:
     def __init__(self, is_radarr):
-        super().__init__()
         self.is_radarr = is_radarr
         self.path_name = getPath(is_radarr, create=True)
 
-    def on_created(self, event):
-        if not event.is_directory and event.src_path.lower().endswith((".torrent", ".magnet")):
-            asyncio.run(on_created(self.is_radarr))
-
-    async def on_run(self):
-        await on_created(self.is_radarr)
+    async def watch(self):
+        async for changes in awatch(self.path_name):
+            for change in changes:
+                if change[0] in [1,2] and (change[1].lower().endswith((".torrent", ".magnet"))):
+                    await on_created(self.is_radarr)
 
 async def main():
-        print("Watching blackhole")
+    print("Watching blackhole")
+    radarr_handler = BlackholeHandler(is_radarr=True)
+    sonarr_handler = BlackholeHandler(is_radarr=False)
 
-        radarr_handler = BlackholeHandler(is_radarr=True)
-        sonarr_handler = BlackholeHandler(is_radarr=False)
-
-        radarr_observer = Observer()
-        radarr_observer.schedule(radarr_handler, radarr_handler.path_name)
-
-        sonarr_observer = Observer()
-        sonarr_observer.schedule(sonarr_handler, sonarr_handler.path_name)
-
-        try:
-            radarr_observer.start()
-            sonarr_observer.start()
-            
-            await asyncio.gather(
-                radarr_handler.on_run(),
-                sonarr_handler.on_run()
-            )
-        except KeyboardInterrupt:
-            radarr_observer.stop()
-            sonarr_observer.stop()
-
-        radarr_observer.join()
-        sonarr_observer.join()
-
+    await asyncio.gather(
+        radarr_handler.watch(),
+        sonarr_handler.watch()
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
